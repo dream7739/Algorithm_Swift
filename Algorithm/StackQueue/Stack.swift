b6e5a8d24cb3a5b158bde52a35a6409130f3e2c8
홍정민 <dream7739@naver.com>
2025-01-09 20:17:50 +0900

    [스택] 스택 구조체 구현

---
+++ Algorithm/StackQueue/Stack.swift
@@ -0,0 +1,50 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 홍정민 on 1/9/25.
+//
+
+import Foundation
+
+// 스택
+// 후입선출 LIFO
+// 삽입, 삭제, 최상단 조회 = O(1)
+// removeLast: 마지막 값이 무조건 존재
+// popLast: 마지막 값이 없다면 nil을 리턴
+struct Stack<T> {
+    private var elements: [T] = []
+    
+    // 최상단
+    var peak: T? {
+        return elements.last
+    }
+    
+    // 비어있는지 유무
+    var isEmpty: Bool {
+        return elements.isEmpty
+    }
+    
+    // 원소 개수
+    var count: Int {
+        return elements.count
+    }
+    
+    // push
+    mutating func push(_ element: T) {
+        elements.append(element)
+    }
+    
+    // pop
+    mutating func pop() -> T? {
+        return elements.popLast()
+    }
+}
+
+var stack = Stack<Int>()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print(stack.pop())
+
